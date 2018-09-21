02e6c6e991f5cce8fd6461761d882c2ddd78bedf
alice <wotupset@gmail.com>
2018-09-21 21:31:55 +0800

    Update 180921-pgsql.php

--- 180921-pgsql.php
+++ 180921-pgsql.php
@@ -1,5 +1,5 @@
 <?php
-//header("content-Type: application/json; charset=utf-8"); //強制
+header("content-Type: application/json; charset=utf-8"); //強制
 
 error_reporting(E_ALL & ~E_NOTICE); //所有錯誤中排除NOTICE提示
 
@@ -16,9 +16,19 @@ print_r($db_url );
   
 $pdo = new PDO( $db_url );
 
-
-               
+if(!$pdo){
+  die('連線失敗');
+}else{
+  echo '連線狀態='.$db->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+  echo "\n";
+}
 }catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
+
+
+echo 'version_php='.phpversion()."\n";
+foreach( $db->query("select version();") as $k => $v ){
+  echo 'version_pgsql='.$v[0]."\n";
+}
 
 
 ?>
