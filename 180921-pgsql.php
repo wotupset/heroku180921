26896bc5a0198b4d41cf7838253274c79ab3cec0
alice <wotupset@gmail.com>
2018-09-21 21:13:46 +0800

    Update 180921-pgsql.php

--- 180921-pgsql.php
+++ 180921-pgsql.php
@@ -3,12 +3,17 @@
 
 error_reporting(E_ALL & ~E_NOTICE); //所有錯誤中排除NOTICE提示
 
-$db = parse_url(getenv("DATABASE_URL"));
+$dbopts = parse_url(getenv("DATABASE_URL"));
+print_r($$dbopts );
 
-print_r($db );
-
-
-
+//pgsql:host=localhost;port=5432;dbname=testdb;user=bruce;password=mypass
+$pdo = new PDO(sprintf("pgsql:host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $dbopts["host"],
+    $dbopts["port"],
+    $dbopts["user"],
+    $dbopts["pass"],
+    ltrim(dbopts["path"], "/")
+));
 
 
 ?>
