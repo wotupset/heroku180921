c4175b1bcf5a52467181912b049a2207a999fef8
alice <wotupset@gmail.com>
2018-09-23 20:18:22 +0800

    Update 180921-pgsql.php

--- 180921-pgsql.php
+++ 180921-pgsql.php
@@ -6,8 +6,12 @@ error_reporting(E_ALL & ~E_NOTICE); //所有錯誤中排除NOTICE提示
 
 $time  =time();
 $time2 =array_sum( explode( ' ' , microtime() ) );
-echo 'php now='.date("Y-m-d H:i:s",$time)."\n";
-echo 'php UTC='.gmdate("Y-m-d H:i:s",$time)."\n";
+date_default_timezone_set("Asia/Taipei");//時區設定
+//date_default_timezone_set("UTC");//時區設定
+$timezone=date_default_timezone_get();
+echo '[php]timezone='.$timezone."\n";
+echo '[php]now='.date("Y-m-d H:i:s",$time)."\n";
+echo '[php]UTC='.gmdate("Y-m-d H:i:s",$time)."\n";
 
 
 ///
@@ -24,7 +28,7 @@ print_r($db_url );
 $db = new PDO( $db_url );
 echo "\n";
 if(!$db){die('連線失敗');}
-//echo '1連線狀態='.$db->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+echo '連線狀態='.$db->getAttribute(PDO::ATTR_CONNECTION_STATUS);
 echo "\n";
 
 }catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
@@ -34,16 +38,16 @@ echo "\n";
 
 
 
-echo 'version_php='.phpversion()."\n";
+echo '[php]version='.phpversion()."\n";
 foreach( $db->query("select version();") as $k => $v ){
-  echo 'version_pgsql='.$v[0]."\n";
+  echo '[pgsql]version='.$v[0]."\n";
 }
 
 
 
 foreach( $db->query("SELECT now()::date, now()::time") as $k => $v ){
   print_r($v);
-  echo 'pgsql_time='.$v[0]." ".$v[1]."\n";
+  echo '[pgsql]now()='.$v[0]." ".$v[1]."\n";
 }
 
 $stmt=$db->query("SELECT CURRENT_DATE,CURRENT_TIME,CURRENT_TIMESTAMP,LOCALTIMESTAMP");
@@ -51,9 +55,9 @@ $stmt=$db->query("SELECT CURRENT_DATE,CURRENT_TIME,CURRENT_TIMESTAMP,LOCALTIMEST
 //while ($row = $stmt->fetch() ){}
 $row = $stmt->fetch();//取回第一筆資料
 print_r($row);
-echo 'pgsql_current_timestamp='.$row['current_timestamp'];
+echo '[pgsql]current_timestamp='.$row['current_timestamp'];
 echo "\n";
-echo 'pgsql_localtimestamp='.$row['localtimestamp'];
+echo '[pgsql]localtimestamp='.$row['localtimestamp'];
 echo "\n";
 
 ?>
